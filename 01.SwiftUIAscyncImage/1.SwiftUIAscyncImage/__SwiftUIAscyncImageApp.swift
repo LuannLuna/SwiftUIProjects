3a45dc76bdadb02aae3ab6e19cf9195426692475
Luann Luna <luann.luna@capgemini.com>
2022-03-30 10:26:58 -0300

    01. Working with async images + load phases + transactions

---
+++ 01.SwiftUIAscyncImage/1.SwiftUIAscyncImage/__SwiftUIAscyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  __SwiftUIAscyncImageApp.swift
+//  1.SwiftUIAscyncImage
+//
+//  Created by Luann Luna on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct __SwiftUIAscyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
